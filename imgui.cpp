e38ce79ea940ad6882a457ccb997b2171300c60d
Ben Carter <ben@shironekolabs.com>
2020-06-08 15:24:15 +0900

    Texture-based round corners: Use separate textures for square corners and fix a bunch of corner-case issues

--- imgui.cpp
+++ imgui.cpp
@@ -7571,6 +7571,7 @@ void ImGui::SetCurrentFont(ImFont* font)
     g.DrawListSharedData.TexUvWhitePixel = atlas->TexUvWhitePixel;
     g.DrawListSharedData.TexUvLines = atlas->TexUvLines;
     g.DrawListSharedData.TexRoundCornerData = &atlas->TexRoundCornerData;
+    g.DrawListSharedData.TexSquareCornerData = &atlas->TexSquareCornerData;
     g.DrawListSharedData.Font = g.Font;
     g.DrawListSharedData.FontSize = g.FontSize;
     g.DrawListSharedData.FontScale = g.FontScale;
